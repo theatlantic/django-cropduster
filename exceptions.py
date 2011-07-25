940f7073509d3cb1cc93a486f0fc5f111dee5dfc
Frankie Dintino <fdintino@theatlantic.com>
2011-07-25 09:29:46 -0400

    Adding exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,17 @@
+class CropDusterException(Exception):
+	pass
+
+class CropDusterUrlException(CropDusterException):
+	pass
+
+class CropDusterViewException(CropDusterException):
+	pass
+
+class CropDusterModelException(CropDusterException):
+	pass
+
+class CropDusterImageException(CropDusterException):
+	pass
+
+class CropDusterFileException(CropDusterException):
+	pass
