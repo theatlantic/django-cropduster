cec999780e798db53f119dfca84a11f123786729
Frankie Dintino <fdintino@theatlantic.com>
2012-08-11 17:23:46 -0400

    Made cropduster v1 submodule into a proper setuptools package

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(
+    name='django-cropduster',
+    version='1.0',
+    author='The Atlantic',
+    author_email='atmoprogrammers@theatlantic.com',
+    url='http://github.com/theatlantic/django-cropduster',
+    description='Image uploader and cropping tool',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'PIL',
+        'jsonutil',
+        'Django>=1.2',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
