3ab5b63c5df5b09e6008cc0f23e8217df8044f13
refefer <astanton@theatlantic.com>
2012-08-14 15:47:31 -0400

    Updates get_image() to actually work again

--- cropduster/templatetags/images.py
+++ cropduster/templatetags/images.py
@@ -7,40 +7,26 @@ from cropduster.models import Size
 
 register = template.Library()
 
-# This is cached across requests. Do we want that?
-image_sizes = Size.objects.all()
-image_size_map = {}
-for size in image_sizes:
-    image_size_map[(size.size_set_id, size.slug)] = size
-
-
 @register.object
 def get_image(image, size_name='large', template_name='image.html', width=None, height=None, **kwargs):
-    if image:
-        image_url = image.thumbnail_url(size_name)
-        if image_url is None or image_url == '':
-            return ''
-        try:
-            image_size = image_size_map[(image.size_set_id, size_name)]
-        except KeyError:
-            return ''
+    if not image:
+        return ""
 
-        kwargs.update({
-            'image_url': image_url,
-            'width': width or image_size.width or '',
-            'height': height or image_size.height  or '',
-            'size_name': size_name,
-            'attribution': image.attribution,
-            'alt': kwargs.get('alt', image.caption),
-        })
-        kwargs['title'] = kwargs.get('title', kwargs['alt'])
-
-        if getattr(settings, 'CROPDUSTER_KITTY_MODE', False):
-            kwargs['image_url'] = "http://placekitten.com/{0}/{1}".format(
-                kwargs['width'], kwargs['height'])
-
-        tpl = get_template('templatetags/' + template_name)
-        ctx = template.Context(kwargs)
-        return tpl.render(ctx)
-    else:
+    thumb = image.get_thumbnail(size_name)
+    if thumb is None:
         return ''
+
+    kwargs.update({
+        'image_url': thumb.get_absolute_url(),
+        'width': width or thumb.width,
+        'height': height or thumb.height,
+        'size_name': thumb.size.name,
+        'attribution': image.metadata.attribution,
+        'alt': kwargs.get('alt', image.metadata.caption),
+    })
+    kwargs['title'] = kwargs.get('title', kwargs['alt'])
+
+    tpl = get_template('templatetags/' + template_name)
+    ctx = template.Context(kwargs)
+    return tpl.render(ctx)
+
