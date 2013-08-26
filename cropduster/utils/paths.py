b5aaf015d562f42d56e162d3bd22d97316cc9078
Frankie Dintino <fdintino@theatlantic.com>
2013-08-26 10:09:30 -0400

    v4 (cropduster.utils): Simplify path utility functions

--- cropduster/utils/paths.py
+++ cropduster/utils/paths.py
@@ -1,9 +1,8 @@
 import os
 import re
-from datetime import datetime
 
 from django.conf import settings
-from django.template.defaultfilters import slugify
+from django.db.models.fields.files import FileField
 
 
 __all__ = ('get_upload_foldername', 'get_media_path', 'get_media_url',
@@ -18,42 +17,19 @@ re_url_slashes = re.compile(r'(?:\A|(?<=/))/')
 re_path_slashes = re.compile(r'(?<=/)/')
 
 
-def get_available_name(dir_name, file_name):
-    """
-    Create a folder based on file_name and return it.
-
-    If a folder with file_name already exists in the given path,
-    create a folder with a unique sequential number at the end.
-    """
-    file_root, extension = os.path.splitext(file_name)
-    file_root = slugify(file_root)
-    name = os.path.join(dir_name, file_root)
-
-    # If the filename already exists, keep adding a higher number
-    # to the folder name until the generated folder doesn't exist.
-    i = 2
-    while os.path.exists(name):
-        # file_ext includes the dot.
-        name = os.path.join(dir_name, file_root + '-' + str(i))
-        i += 1
-    os.makedirs(name)
-
-    return name
-
-
-def get_upload_foldername(file_name, upload_to=None):
+def get_upload_foldername(file_name, upload_to='%Y/%m'):
     # Generate date based path to put uploaded file.
-    date_path = datetime.now().strftime('%Y/%m')
-
-    paths = filter(None, [settings.MEDIA_ROOT, upload_to, date_path])
-    upload_path = os.path.join(*paths)
-
-    # Make sure upload_path exists.
-    if not os.path.exists(upload_path):
-        os.makedirs(upload_path)
-
-    # Get available name and return.
-    return get_available_name(upload_path, file_name)
+    file_field = FileField(upload_to=upload_to)
+    filename = file_field.generate_filename(None, file_name)
+    filename = re.sub(r'[_\-]+', '_', filename)
+    root_dir = os.path.splitext(filename)[0]
+    root_dir = dir_name = os.path.join(settings.MEDIA_ROOT, root_dir)
+    i = 1
+    while os.path.exists(dir_name):
+        dir_name = u'%s-%d' % (root_dir, i)
+        i += 1
+    os.makedirs(dir_name)
+    return dir_name
 
 
 def get_media_path(url):
@@ -62,10 +38,12 @@ def get_media_path(url):
     return re_path_slashes.sub('', path)
 
 
-def get_relative_media_url(path):
+def get_relative_media_url(path, clean_slashes=True):
     """Determine system file's media URL without MEDIA_URL prepended."""
     url = re_media_root.sub('', path)
-    return re_url_slashes.sub('', url)
+    if clean_slashes:
+        url = re_url_slashes.sub('', url)
+    return url
 
 
 def get_media_url(path):
