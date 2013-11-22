b2227b892ca673512394c63cf763c90254517f44
Josh West <joshwest@theatlantic.com>
2013-11-22 10:20:10 -0500

    Resize thumbnails in two steps to increase performance

--- cropduster/utils/__init__.py
+++ cropduster/utils/__init__.py
@@ -1,6 +1,7 @@
 from .image import (
     get_image_extension, is_transparent, exif_orientation,
-    correct_colorspace, is_animated_gif, has_animated_gif_support, process_image)
+    correct_colorspace, is_animated_gif, has_animated_gif_support, process_image,
+    smart_resize)
 from .paths import get_upload_foldername, get_media_path, get_relative_media_url
 from .sizes import get_min_size
 from . import jsonutils as json
