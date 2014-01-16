de8f7511f18c4c565b62de39a4059511ebb99107
Jason Goldstein <jason@betheshoe.com>
2014-01-16 11:53:14 -0500

    A templatetag for cropduster v3 that mirrors the one we use in v4. Since v3->4 migrators are basically ready to go, it seems dumb to start referring to images in a way that won't be forwards-compatible.

--- cropduster3/templatetags/cropdusterimages.py
+++ cropduster3/templatetags/cropdusterimages.py
@@ -28,3 +28,35 @@ def get_image(image, size_name='large', template_name='image.html',
     tpl = template.loader.get_template('templatetags/' + template_name)
     ctx = template.Context(kwargs)
     return tpl.render(ctx)
+
+
+@register.assignment_tag
+def get_v3_crop(image, crop_name, size=None):
+    """
+    This uses the same API as the v4 get_crop for easy upgrading.
+    
+    Get the crop of an image. Usage:
+
+    {% get_v3_crop article.image 'square_thumbnail' as size=1 %}
+
+    will return a dictionary of
+
+    {
+        "url": /media/path/to/my.jpg,
+        "width": 150,
+        "height" 150,
+    }
+
+    For use in an image tag or style block.
+
+    Omitting the `size` kwarg will omit width and height. You usually want to do this,
+    since the size lookup is a database call.
+    """
+    data = {}
+    crop = image.get_thumbnail(crop_name)
+    if crop:
+        data['url'] = crop.image.url
+    if size:
+        data['width'] = crop.width
+        data['height'] = crop.height
+    return data
