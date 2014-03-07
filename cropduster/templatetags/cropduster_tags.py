ec36d48e70d5132687a6b1c14fd60ca0858bb633
Carl Johnson <cjohnson@theatlantic.com>
2014-03-07 14:42:04 -0500

    Template tag: Add option to get attribution

--- cropduster/templatetags/cropduster_tags.py
+++ cropduster/templatetags/cropduster_tags.py
@@ -3,11 +3,11 @@ from cropduster.models import Image
 register = template.Library()
 
 @register.assignment_tag
-def get_crop(image, crop_name, size=None):
+def get_crop(image, crop_name, size=None, attribution=None):
     """
     Get the crop of an image. Usage:
 
-    {% get_crop article.image 'square_thumbnail' size=1 as img %}
+    {% get_crop article.image 'square_thumbnail' size=1 attribution=1 as img %}
 
     will a dictionary to `img` that looks like:
 
@@ -15,6 +15,9 @@ def get_crop(image, crop_name, size=None):
         "url": /media/path/to/my.jpg,
         "width": 150,
         "height" 150,
+        "attribution": 'Stock Photoz',
+        "attribution_link": 'http://stockphotoz.com',
+        "caption": 'Woman laughing alone with salad.',
     }
 
     For use in an image tag or style block like:
@@ -24,9 +27,17 @@ def get_crop(image, crop_name, size=None):
     Omitting the `size` kwarg will omit width and height. You usually want to do this,
     since the size lookup is a database call.
 
+    Omitting the `attribution` kwarg will omit the attribution, attribution_link,
+    and caption.
     """
     data = {}
     data['url'] = getattr(Image.get_file_for_size(image, crop_name), 'url', None)
     if size:
         data['width'], data['height'] = image.related_object.get_image_size(size_name=crop_name)
+    if attribution:
+        data.update({
+            "attribution": image.related_object.attribution,
+            "attribution_link": image.related_object.attribution_link,
+            "caption": image.related_object.caption,
+        })
     return data
