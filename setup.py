449d87fd7feee5082dbd18d07e9761efd35ce7c7
Jason Goldstein <jason@betheshoe.com>
2014-03-27 14:15:33 -0400

    v4.4.17

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-cropduster',
-    version='4.4.16',
+    version='4.4.17',
     author='The Atlantic',
     author_email='atmoprogrammers@theatlantic.com',
     url='http://github.com/theatlantic/django-cropduster',
