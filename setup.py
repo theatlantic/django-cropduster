8eb85007e041e963cb4b585c5347634bc9c64965
Frankie Dintino <fdintino@theatlantic.com>
2014-06-06 08:16:34 -0400

    Bump version to v4.5.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-cropduster',
-    version='4.5.0',
+    version='4.5.1',
     author='The Atlantic',
     author_email='atmoprogrammers@theatlantic.com',
     url='http://github.com/theatlantic/django-cropduster',
