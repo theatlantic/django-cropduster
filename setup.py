f64ec4b2cac9ec5d86ceb2196bcc9a1d300ea32d
Jason Goldstein <jason@betheshoe.com>
2015-01-26 16:47:33 -0500

    v4.7.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except IOError:
 
 setup(
     name='django-cropduster',
-    version='4.7.6',
+    version='4.7.7',
     author='The Atlantic',
     author_email='programmers@theatlantic.com',
     url='https://github.com/theatlantic/django-cropduster',
