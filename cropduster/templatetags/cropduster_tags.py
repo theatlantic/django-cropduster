a3001cfd7bbc637ee95c3dd1dc7c1a94895d3bc9
Chris Barna <chris@unbrain.net>
2015-03-23 13:13:21 -0400

    Remove all kwargs from get_crop.

--- cropduster/templatetags/cropduster_tags.py
+++ cropduster/templatetags/cropduster_tags.py
@@ -11,11 +11,11 @@ register = template.Library()
 
 
 @register.assignment_tag
-def get_crop(image, crop_name, size=None, attribution=None, exact_size=False):
+def get_crop(image, crop_name, **kwargs):
     """
     Get the crop of an image. Usage:
 
-    {% get_crop article.image 'square_thumbnail' attribution=1 exact_size=1 as img %}
+    {% get_crop article.image 'square_thumbnail' as img %}
 
     will assign to `img` a dictionary that looks like:
 
@@ -31,25 +31,13 @@ def get_crop(image, crop_name, size=None, attribution=None, exact_size=False):
     For use in an image tag or style block like:
 
         <img src="{{ img.url }}">
-
-    The `size` kwarg is deprecated.
-
-    Omitting the `attribution` kwarg will omit the attribution, attribution_link,
-    and caption.
-
-    Omitting the `exact_size` kwarg will return the width and/or the height of
-    the crop size that was passed in. Crop sizes do not always require both
-    values so `exact_size` gives you access to the actual size of an image.
     """
 
     if not image or not image.related_object:
         return
 
-    if size:
-        warnings.warn("The size kwarg is deprecated.", DeprecationWarning)
-
-    if exact_size:
-        warnings.warn("The exact_size kwarg is deprecated.", DeprecationWarning)
+    if len(kwargs) > 0:
+        warnings.warn("All get_crop kwargs have been deprecated", DeprecationWarning)
 
     data = {}
     try:
@@ -60,14 +48,12 @@ def get_crop(image, crop_name, size=None, attribution=None, exact_size=False):
         else:
             return data
 
-    data["url"] = thumb.url
-    data["width"] = thumb.width
-    data["height"] = thumb.height
-
-    if attribution and image.related_object:
-        data.update({
-            "attribution": image.related_object.attribution,
-            "attribution_link": image.related_object.attribution_link,
-            "caption": image.related_object.caption,
-        })
+    data.update({
+        "url": thumb.url,
+        "width": thumb.width,
+        "height": thumb.height,
+        "attribution": image.related_object.attribution,
+        "attribution_link": image.related_object.attribution_link,
+        "caption": image.related_object.caption,
+    })
     return data
