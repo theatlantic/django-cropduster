27158afc0d2636c058eb55a897dae4c4df6a4249
Frankie Dintino <fdintino@theatlantic.com>
2015-05-31 11:43:18 -0400

    Simplify runtests.py by moving settings into cropduster.tests.settings

---
+++ cropduster/tests/settings.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import os
+import tempfile
+
+import django
+
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+temp_dir = tempfile.mkdtemp()
+
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
+SECRET_KEY = 'z-i*xqqn)r0i7leak^#clq6y5j8&tfslp^a4duaywj2$**s*0_'
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+INSTALLED_APPS = (
+    'grappelli',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'generic_plus',
+    'cropduster',
+)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+SITE_ID = 1
+ROOT_URLCONF = 'cropduster.tests.urls'
+MEDIA_ROOT = os.path.join(temp_dir, 'media')
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+DEBUG_PROPAGATE_EXCEPTIONS = True
+TEST_RUNNER = 'django.test.runner.DiscoverRunner' if django.VERSION >= (1, 6) else 'discover_runner.runner.DiscoverRunner'
+TEMPLATE_DIRS = (
+    os.path.join(current_dir, 'templates'),
+)
--- runtests.py
+++ runtests.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-import django
 import os
 import sys
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cropduster.tests.settings'
+
+
+import django
 from django.core.management import execute_from_command_line
-
-from django.conf import settings, global_settings as default_settings
-
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-test_dir = os.path.join(current_dir, 'test')
 
 
 # Give feedback on used versions
@@ -16,53 +15,6 @@ sys.stderr.write('Using Python version %s from %s\n' % (sys.version[:5], sys.exe
 sys.stderr.write('Using Django version %s from %s\n' % (
     django.get_version(),
     os.path.dirname(os.path.abspath(django.__file__))))
-
-if not settings.configured:
-    settings.configure(**{
-        'DEBUG': True,
-        'TEMPLATE_DEBUG': True,
-        'DATABASES': {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:'
-            }
-        },
-        'TEMPLATE_LOADERS': (
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader',
-        ),
-        'TEMPLATE_CONTEXT_PROCESSORS': default_settings.TEMPLATE_CONTEXT_PROCESSORS + (
-            'django.core.context_processors.request',
-        ),
-        'INSTALLED_APPS': (
-            'grappelli',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.messages',
-            'django.contrib.sessions',
-            'django.contrib.sites',
-            'django.contrib.staticfiles',
-            'django.contrib.admin',
-            'generic_plus',
-            'cropduster',
-        ),
-        'MIDDLEWARE_CLASSES': (
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-        ),
-        'SITE_ID': 1,
-        'ROOT_URLCONF': 'cropduster.tests.urls',
-        'MEDIA_ROOT': os.path.join(test_dir, 'media'),
-        'MEDIA_URL': '/media/',
-        'STATIC_URL': '/static/',
-        'DEBUG_PROPAGATE_EXCEPTIONS': True,
-        'TEST_RUNNER': 'django.test.runner.DiscoverRunner' if django.VERSION >= (1, 6) else 'discover_runner.runner.DiscoverRunner',
-        'TEMPLATE_DIRS': (
-            os.path.join(current_dir, 'cropduster', 'tests', 'templates'),
-        )
-    })
-
 
 def runtests():
     argv = sys.argv[:1] + ['test', 'cropduster', '--traceback', '--verbosity=1'] + sys.argv[1:]
