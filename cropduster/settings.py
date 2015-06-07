94f0c2a2528d8857c202b486afd679684e75804b
Frankie Dintino <fdintino@theatlantic.com>
2015-06-07 13:50:54 -0400

    Rename 'get_jpeg_quality' setting to 'CROPDUSTER_JPEG_QUALITY'
    Also, allow it to be either a callable or numeric

--- cropduster/settings.py
+++ cropduster/settings.py
@@ -1,7 +1,9 @@
+import six
 import math
 import PIL
 from distutils.version import LooseVersion
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 CROPDUSTER_MEDIA_ROOT = getattr(settings, 'CROPDUSTER_MEDIA_ROOT', settings.MEDIA_ROOT)
@@ -20,7 +22,7 @@ CROPDUSTER_PREVIEW_WIDTH = getattr(settings, 'CROPDUSTER_PREVIEW_WIDTH', 800)
 CROPDUSTER_PREVIEW_HEIGHT = getattr(settings, 'CROPDUSTER_PREVIEW_HEIGHT', 500)
 
 
-def get_jpeg_quality(width, height):
+def default_jpeg_quality(width, height):
     p = math.sqrt(width * height)
     if p >= 1750:
         return 80
@@ -29,7 +31,17 @@ def get_jpeg_quality(width, height):
     else:
         return 90
 
-get_jpeg_quality = getattr(settings, 'get_jpeg_quality', get_jpeg_quality)
+CROPDUSTER_JPEG_QUALITY = getattr(settings, 'CROPDUSTER_JPEG_QUALITY', default_jpeg_quality)
+
+def get_jpeg_quality(width, height):
+    if six.callable(CROPDUSTER_JPEG_QUALITY):
+        return CROPDUSTER_JPEG_QUALITY(width, height)
+    elif isinstance(CROPDUSTER_JPEG_QUALITY, (int, float)):
+        return CROPDUSTER_JPEG_QUALITY
+    else:
+        raise ImproperlyConfigured(
+            "CROPDUSTER_JPEG_QUALITY setting must be either a callable "
+            "or a numeric value, got type %s" % (type(CROPDUSTER_JPEG_QUALITY).__name__))
 
 JPEG_SAVE_ICC_SUPPORTED = (LooseVersion(getattr(PIL, 'PILLOW_VERSION', '0'))
     >= LooseVersion('2.2.1'))
