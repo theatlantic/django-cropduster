14b1e901b82e88729dbedf5823df45e5399fa2ca
Frankie Dintino <fdintino@theatlantic.com>
2017-02-15 12:00:46 -0500

    v4.10.3 [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-cropduster',
-    version='4.10.2',
+    version='4.10.3',
     author='The Atlantic',
     author_email='programmers@theatlantic.com',
     url='https://github.com/theatlantic/django-cropduster',
