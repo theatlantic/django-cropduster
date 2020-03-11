1afbbe071acc7dd54ef9fd19897e7dc443d2a443
Frankie Dintino <fdintino@gmail.com>
2020-03-11 12:28:14 -0400

    Allow testing of s3 boto file storage backend

--- cropduster/tests/helpers.py
+++ cropduster/tests/helpers.py
@@ -1,8 +1,13 @@
+from io import open
+import tempfile
 import os
 import shutil
 import uuid
 
 from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.test import override_settings
 
 PATH = os.path.split(__file__)[0]
 ORIG_IMG_PATH = os.path.join(PATH, 'data')
@@ -10,35 +15,45 @@ ORIG_IMG_PATH = os.path.join(PATH, 'data')
 
 class CropdusterTestCaseMediaMixin(object):
 
+    def _pre_setup(self):
+        super(CropdusterTestCaseMediaMixin, self)._pre_setup()
+        new_settings = {}
+        self.temp_media_root = tempfile.mkdtemp(prefix='TEST_MEDIA_ROOT_')
+
+        storage_cls = settings.DEFAULT_FILE_STORAGE
+
+        if storage_cls == 'django.core.files.storage.FileSystemStorage':
+            new_settings['MEDIA_ROOT'] = self.temp_media_root
+        elif storage_cls != 'storages.backends.s3boto3.S3Boto3Storage':
+            raise Exception("Unsupported DEFAULT_FILE_STORAGE %s" % storage_cls)
+        self.override = override_settings(**new_settings)
+        self.override.enable()
+
+    def _post_teardown(self):
+        if hasattr(default_storage, 'bucket'):
+            default_storage.bucket.objects.filter(Prefix=default_storage.location).delete()
+        else:
+            shutil.rmtree(self.temp_media_root)
+
+        self.override.disable()
+
+        super(CropdusterTestCaseMediaMixin, self)._post_teardown()
+
     def setUp(self):
         super(CropdusterTestCaseMediaMixin, self).setUp()
 
         random = uuid.uuid4().hex
-        self.TEST_IMG_ROOT = os.path.join(settings.MEDIA_ROOT, random)
-        self.TEST_IMG_DIR = os.path.join(self.TEST_IMG_ROOT, 'data')
+        self.TEST_IMG_DIR = ORIG_IMG_PATH
         self.TEST_IMG_DIR_RELATIVE = os.path.join(random, 'data')
-
-        # Create directory for test images
-        shutil.copytree(ORIG_IMG_PATH, self.TEST_IMG_DIR)
-
-    def tearDown(self):
-        super(CropdusterTestCaseMediaMixin, self).tearDown()
-
-        # Remove all generated images
-        shutil.rmtree(self.TEST_IMG_ROOT, ignore_errors=True)
 
     def create_unique_image(self, image):
         image_uuid = uuid.uuid4().hex
-        image_dir = os.path.join(self.TEST_IMG_DIR, image_uuid)
-
-        if not os.path.exists(image_dir):
-            os.makedirs(image_dir)
 
         ext = os.path.splitext(image)[1]
         image_name = os.path.join(
             self.TEST_IMG_DIR_RELATIVE, image_uuid, "original%s" % ext)
 
-        shutil.copyfile(
-            os.path.join(self.TEST_IMG_DIR, image),
-            os.path.join(settings.MEDIA_ROOT, image_name))
+        with open("%s/%s" % (ORIG_IMG_PATH, image), mode='rb') as f:
+            default_storage.save(image_name, ContentFile(f.read()))
+
         return image_name
--- cropduster/tests/settings.py
+++ cropduster/tests/settings.py
@@ -1,4 +1,6 @@
+import os
 import django
+import uuid
 
 from selenosis.settings import *
 
@@ -20,3 +22,11 @@ INSTALLED_APPS += (
 ROOT_URLCONF = 'cropduster.tests.urls'
 
 TEMPLATES[0]['OPTIONS']['debug'] = True
+
+if os.environ.get('S3') == '1':
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_STORAGE_BUCKET_NAME = 'ollie-cropduster-media-test-bucket-dev'
+    AWS_DEFAULT_ACL = None
+    AWS_LOCATION = 'cropduster/%s/' % uuid.uuid4().hex
+else:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
